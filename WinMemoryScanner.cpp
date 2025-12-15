8d49f2ff741807594737169235b8b38b700412b5
FrenzoSource <enzojeannin49@outlook.com>
2025-12-15 17:56:46 +0100

    Add files via upload

--- WinMemoryScanner.cpp
+++ WinMemoryScanner.cpp
@@ -4,31 +4,75 @@
 
 
 
-
-int main() {
+//ouverture d'un processus grace au PID
+HANDLE openProcess_pid() {
     DWORD pid;
-    std::cout << "Veuillez saisir le PID du processus : ";
+    std::cout << "Veuillez saisir un pid : ";
     std::cin >> pid;
 
-    //ouverture du processus
     HANDLE h = OpenProcess(PROCESS_QUERY_INFORMATION, false, pid);
 
-    if (h == NULL) {
-        std::cout << "Echec. Erreur : " << GetLastError() << std::endl;
+    return h;
+}
+
+
+
+
+void affichage_erreur(DWORD num_erreur) {
+    std::cout << "ERREUR !!" << std::endl;
+    if (num_erreur == 5) std::cout << "Acces refusée. Vous ne disposez pas les droits necessaires." << std::endl;
+    else if (num_erreur == 87) std::cout << "PID ou parametre invalides" << std::endl;
+    else if (num_erreur == 299) std::cout << "Acces memoire limite par protection." << std::endl;
+}
+
+
+void affichage_chemin_proc(HANDLE proc, char * tampon, DWORD taille) {
+    
+    if (QueryFullProcessImageNameA(proc, 0, tampon, &taille)) {
+        std::cout << "Chemin : " << tampon << std::endl;
+    }
+    else affichage_erreur(GetLastError());
+}
+
+
+
+void affichage_etat_proc(HANDLE proc, DWORD & code_sortie) {
+    if (GetExitCodeProcess(proc, &code_sortie)) {
+        if (code_sortie == STILL_ACTIVE) {   // STILL_ACTIVE est défénie comme :     define STILL_ACTIVE 259
+            std::cout << "Processus toujours en cours d'execution." << std::endl;
+        }
+        else std::cout << "Processus terminée. Code de sortie : " << code_sortie << std::endl;
+    }
+    else affichage_erreur(GetLastError());
+}
+
+
+
+int main() {
+
+    //ouverture d'un processus  pour récuperer un HANDLE
+    HANDLE proc1 = openProcess_pid();
+    if (proc1 == NULL) {    //Important pour la gestion des erreurs
+        affichage_erreur(GetLastError());
         return 1;
     }
 
 
-    char buffer[MAX_PATH];   // tampon 
-    DWORD size = MAX_PATH;
 
-    if (QueryFullProcessImageNameA(h, 0, buffer, &size)) {
-        std::cout << "Chemin du processus : " << buffer << std::endl;
-    }
-    else {
-        std::cout << "Impossible d'obtenir le chemin. Erreur : " << GetLastError() << std::endl;
-    };
-    CloseHandle(h);  // TOUJOURS FERMER
+    //MAX_PATH est défini dans la WinApi et vaut 260
+    char tampon_proc1[MAX_PATH];
+    DWORD taille_tampon_proc1 = MAX_PATH;
+
+    affichage_chemin_proc(proc1, tampon_proc1, taille_tampon_proc1);
+
+
+    
+    DWORD exitCode = 0;
+    affichage_etat_proc(proc1, exitCode);
+
+
+
+    CloseHandle(proc1);  // TOUJOURS FERMER
 
     return 0;
 
